5816c4b108186463fdd81c23670877c38c0c72c5
geekbrucy <peichunzhou1006@gmail.com>
2018-07-06 16:07:25 +1000

    filter finished desktop style finished

--- assets/js/content.js
+++ assets/js/content.js
@@ -12,12 +12,18 @@ var projModule = (function () {
     grid: '#E84E34'
   }
   var contentContainer = document.getElementsByClassName('content-container')[0]
+  var filterResult = document.getElementsByClassName('filter-result')[0]
+  var filterItemContainer = filterResult.getElementsByClassName('items-container')[0]
+  var allItems = []
+  var filterArr = [] // remove duplicated tech
+
   // Read local json data
   var getData = async function () {
     var res = await fetch('./data/proj.json')
     var data = await res.json()
     var len = data.length
     attachToDomStr(data, len)
+    handleFilter()
   }
 
   // Remove repetitive category for collapse list
@@ -31,29 +37,115 @@ var projModule = (function () {
     return arr
   }
 
+  // click filter event callback
+  var filterClick = function () {
+    var filteredItems = [] // contains filtered DOM items
+    filterItemContainer.innerHTML = ''
+    this.clicked = this.clicked === false ? true : false
+    var tags = this.parentNode.children
+    var len = tags.length
+    var counter = len
+    if (this.clicked) {
+      if(colors[this.innerText]) {
+        this.style.backgroundColor = colors[this.innerText]
+      } else {
+        this.style.backgroundColor = '#2ab7ca'
+      }
+    } else {
+      this.style.backgroundColor = '#0F1014'
+    }
+    for (var i = 0; i < len; i++) {
+      if (tags[i].clicked) {
+        counter--
+        console.log(tags[i].innerText)
+        filteredItems = filteredItems.length <= 0 ? allItems.filter(function (el) {
+          if (el.tech.indexOf(tags[i].innerText) >= 0) {
+            return el
+          }
+        }) : filteredItems.filter(function (el) {
+          if (el.tech.indexOf(tags[i].innerText) >= 0) {
+            return el
+          }
+        })
+      } 
+    }
+    if (counter === len) {
+      var cateTitle = document.getElementsByClassName('cate-title')
+      // console.log(allItems)
+      for (var i = 0; i < cateTitle.length; i++) {
+        var nextSib = cateTitle[i].nextElementSibling
+        console.log(cateTitle[i])
+        console.log(cateTitle[i].innerText)
+        console.log(nextSib)
+        var matchedArr = allItems.filter(function (el) {
+          // console.log(el)
+          if (cateTitle[i].innerHTML.indexOf(el.category) !== -1) {
+            console.log(el)
+            return el
+          }
+        })
+
+        matchedArr.map(function (el) {
+          nextSib.appendChild(el.dom)
+        })
+      }
+      contentContainer.classList.add('activated')
+      filterResult.classList.remove('activated')
+    } else {
+      filterResult.classList.add('activated')
+      contentContainer.classList.remove('activated')
+    }
+    for (var j = 0; j < filteredItems.length; j++) {
+      filterItemContainer.appendChild(filteredItems[j].dom)
+    }
+  }
+
+  // handle filter section
+  var handleFilter = function () {
+    var filterContainer = document.getElementsByClassName('content-filter')[0]
+    var len = filterArr.length
+    for (var i = 0; i < len; i++) {
+      var span = document.createElement('span')
+      var techClass = 'tech-' + filterArr[i]
+      span.className = 'tech ' + techClass
+      // span.style.backgroundColor = colors[filterArr[i]]
+      span.innerText = filterArr[i]
+      span.clicked = false
+      span.addEventListener('click', filterClick, false)
+      filterContainer.appendChild(span)
+    }
+  }
+
   // handle tech field in data, and bind to element
   var getDataTech = function (data, parentNode) {
-    if (data.tech) {
+    if (data) {
       var techContainer = document.createElement('div')
       techContainer.classList.add('item-tech')
-      var techArr = data.tech
+      var techArr = data
       var len = techArr.length
       var elemArr = []
+
       for (var i = 0; i < len; i++) {
         var span = document.createElement('span')
         var techClass = 'tech-' + techArr[i]
         span.className = 'tech ' + techClass
         span.style.backgroundColor = colors[techArr[i]]
+        if (filterArr.indexOf(techArr[i]) < 0) {
+          filterArr.push(techArr[i])
+        }
         span.innerText = techArr[i]
         elemArr.push(span)
       }
-     elemArr.map(function(el) {
-      techContainer.appendChild(el)
-     })
-     parentNode.appendChild(techContainer)
+      elemArr.map(function (el) {
+        techContainer.appendChild(el)
+      })
+      if (parentNode) {
+        parentNode.appendChild(techContainer)
+      }
     }
   }
-  function clickToExpand () {
+
+  function clickToExpand() {
     this.nextElementSibling.classList.toggle('open')
   }
   // Attach Data to DOM
@@ -105,7 +197,7 @@ var projModule = (function () {
         itemOverlay.appendChild(itemLink)
         itemOverlay.appendChild(itemDesc)
         // handle tech array
-        getDataTech(temp[j], itemOverlay)
+        getDataTech(temp[j].tech, itemOverlay)
 
         itemDiv.appendChild(itemOverlay)
 
@@ -114,6 +206,14 @@ var projModule = (function () {
         itemLink.target = 'blank'
         itemLink.classList.add('item-link')
         itemLink.innerHTML = temp[j].name + '<i class="fas fa-link"></i>'
+        
+        // push into allItems, for easy filter
+        var item = {
+          dom: itemDiv,
+          tech: temp[j].tech,
+          category: cate[i]
+        }
+        allItems.push(item)
 
         // itemDesc settings
         itemDesc.classList.add('item-desc')
